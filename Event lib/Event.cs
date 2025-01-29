25a4c56aaf3e44cd12786b3ad8d09a0a4a339816
xiao <79725759@qq.com>
2025-01-29 18:17:09 +0100

    Add project files.

---
+++ Event lib/Event.cs
@@ -0,0 +1,8 @@
+﻿namespace Event_lib
+{
+    public class Event
+    {
+        public string EventName { get; set; }
+
+    }
+}
---
+++ Event lib/EventRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_lib
+{
+    public class EventRepository
+    {
+
+        public List<Event> GetEvents()
+        {
+            List<Event> events = new List<Event>();
+            events.Add(new Event { EventName = "Event 1" });
+            events.Add(new Event { EventName = "Event 2" });
+            events.Add(new Event { EventName = "Event 3" });
+            events.Add(new Event { EventName = "Event 4" });
+            events.Add(new Event { EventName = "Event 5" });
+            return events;
+        }
+
+        public void getEventDetails(Event e)
+        {
+            Console.WriteLine("Event Name: " + e.EventName);
+        }
+    }
+}
