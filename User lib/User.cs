7f087caf5edce500741afb6ae1c00a2b87701ff0
Glenning <glenning.thor@gmail.com>
2025-01-30 12:47:47 +0100

    Added User lib

---
+++ User lib/User.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace User_lib
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? userId { get; set; }
+
+        [BsonElement("user_name"), BsonRepresentation(BsonType.String)]
+        public required string UserName { get; set; }
+
+        [BsonElement("email"), BsonRepresentation(BsonType.String)]
+        public string? Email { get; set; } = null;
+
+        [BsonElement("is_verified"), BsonRepresentation(BsonType.Boolean)]
+        public bool IsVerified => !string.IsNullOrEmpty(Email);
+
+        [BsonElement("user_role"), BsonRepresentation(BsonType.String)]
+        public UserType UserRole { get; set; } = UserType.Guest;
+
+        public string? ProfilePicture { get; set; }
+    }
+    public enum UserType
+    {
+        Guest,
+        Organizer
+    }
+}
