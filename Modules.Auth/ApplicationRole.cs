c29c0c580caf876a95ecd0dfc2341429e72a259d
xiao <79725759@qq.com>
2025-02-06 12:29:30 +0100

    add auth

---
+++ Modules.Auth/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace Modules.Auth
+{
+    [CollectionName("roles")]
+    public class ApplicationRole : MongoIdentityRole<Guid>
+    {
+
+    }
+}
---
+++ Modules.Auth/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDB.Driver;
+using MongoDbGenericRepository.Attributes;
+
+namespace Modules.Auth
+{
+    [CollectionName("users")]
+    public class Applicationuser : MongoIdentityUser<Guid>
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
